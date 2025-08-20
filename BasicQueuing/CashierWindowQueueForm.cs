c669520d4e78db34d84a3aa5fb9e213f7ecb6ffc
dncdnl <danicadaniela.cesicar@gmail.com>
2025-08-20 19:45:21 +0800

    Removing the error for DisplayCashierQueue

--- BasicQueuing/CashierWindowQueueForm.cs
+++ BasicQueuing/CashierWindowQueueForm.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -22,5 +24,16 @@ namespace BasicQueuing
             // Clear the list box before displaying the queue
             DisplayCashierQueue(CashierClass.CashierQueue);
         }
+
+            // This method will display the cashier queue in the list box
+            public void DisplayCashierQueue(IEnumerable CashierList)
+        {
+            listCashierQueue.Items.Clear();
+            foreach (object obj in CashierList)
+            {
+                listCashierQueue.Items.Add(obj.ToString());
+            }
+        }
+
     }
 }
