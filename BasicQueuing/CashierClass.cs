4a05571c02cd1fee98d5baabf280b76312df75d8
dncdnl <danicadaniela.cesicar@gmail.com>
2025-08-20 18:58:18 +0800

    putting a block of codes inside the Cashier Calss

---
+++ BasicQueuing/CashierClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicQueuing
+{
+
+    internal class CashierClass
+    {
+        private int x;
+        public static string getnumberInQueue = "";
+        public static Queue<string> CashierQueue;
+        public CashierClass()
+        {
+            // Initialize the queue and the counter
+            x = 10000;
+            CashierQueue = new Queue<string>();
+        }
+
+
+    public string CashierGeneratedNumber(string CashierNumber)
+        {
+            x++;
+            CashierNumber = CashierNumber + x.ToString();
+            return CashierNumber;
+        }
+    }
+}
