1d5410b9b9f8946cde92732ac96cc3926b36815e
dncdnl <danicadaniela.cesicar@gmail.com>
2025-08-20 19:09:28 +0800

    Initializing the Cashier Class

--- BasicQueuing/Form1.cs
+++ BasicQueuing/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,21 @@ using System.Windows.Forms;
 
 namespace BasicQueuing
 {
+
     public partial class QueuingForm : Form
     {
+        private CashierClass cashier;
         public QueuingForm()
         {
             InitializeComponent();
+            // Initializing the CashierClass
+            cashier = new CashierClass();
         }
 
         private void btnCashier_Click(object sender, EventArgs e)
         {
-            // calling and declaring the cashier class
-            CashierClass cashier = new CashierClass();
+            
+            
         }
     }
 }
